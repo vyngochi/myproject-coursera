0ab6245858e02a156c07552ef2d1e2cc12213244
vyngochi <ncv.ngovy@gmail.com>
2025-05-29 00:35:00 +0700

    pdo

--- pdo.php
+++ pdo.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'shinkansen.proxy.rlwy.net';
-$port = '3306';
-$db   = 'misc';
+$host = 'mainline.proxy.rlwy.net';
+$port = '51274';
+$db = 'misc';
 $user = 'fred';
 $pass = 'zap';
+ 
 
-$pdo = null;
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
